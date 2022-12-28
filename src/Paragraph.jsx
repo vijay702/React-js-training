9ee065184bb5b4da3c23bbacb5e3d9866c4e55aa
AravindRamesh07 <aravind.r@eminds.ai>
2022-12-28 23:56:37 +0530

    changes done

---
+++ src/Paragraph.jsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+
+const Paragraph = () => {
+  return (
+    <div>
+  <table >
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+</table>
+    </div>
+  );
+}
+
+export default Paragraph
